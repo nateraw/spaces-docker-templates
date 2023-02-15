d9bb5bba9486a84465b0923653d5cada37e7aeca
nateraw <nxr9266@g.rit.edu>
2023-02-15 03:21:58 -0500

    :busts_in_silhouette: add authors field for attribution

--- app/app.py
+++ app/app.py
@@ -1,18 +1,29 @@
 import gradio as gr
 
 
-def show_template(name, description, author, url, image_url, more_info=None):
+def name_to_hf_markdown(name):
+    return f"[{name}](https://huggingface.co/{name})"
+
+
+def show_template(name, description, authors, url, image_url, more_info=None):
+    if isinstance(authors, str):
+        authors = [authors]
+    authors_md = ", ".join([name_to_hf_markdown(author) for author in authors])
     with gr.Box():
         with gr.Row():
             with gr.Column(scale=1):
                 gr.HTML(f"""<img src="{image_url}" alt="{name}-thumbnail" height=256 width=256>""")
             with gr.Column(scale=4):
-                gr.Markdown(f"""
+                gr.Markdown(
+                    f"""
                 ## {name}
                 [![Open In Colab](https://img.shields.io/badge/-Duplicate%20Space-blue?labelColor=white&amp;style=flat&amp;logo=data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAAAXNSR0IArs4c6QAAAP5JREFUOE+lk7FqAkEURY+ltunEgFXS2sZGIbXfEPdLlnxJyDdYB62sbbUKpLbVNhyYFzbrrA74YJlh9r079973psed0cvUD4A+4HoCjsA85X0Dfn/RBLBgBDxnQPfAEJgBY+A9gALA4tcbamSzS4xq4FOQAJgCDwV2CPKV8tZAJcAjMMkUe1vX+U+SMhfAJEHasQIWmXNN3abzDwHUrgcRGmYcgKe0bxrblHEB4E/pndMazNpSZGcsZdBlYJcEL9Afo75molJyM2FxmPgmgPqlWNLGfwZGG6UiyEvLzHYDmoPkDDiNm9JR9uboiONcBXrpY1qmgs21x1QwyZcpvxt9NS09PlsPAAAAAElFTkSuQmCC&amp;logoWidth=14)]({url})
 
                 #### {description}
-            """)
+
+                **Author(s):** {authors_md}
+            """
+                )
                 if more_info:
                     with gr.Row():
                         with gr.Accordion("👀 More Details", open=False):
@@ -38,7 +49,7 @@ with gr.Blocks(css="style.css") as demo:
     show_template(
         name="JupyterLab",
         description="Spin up a JupyterLab instance with just a couple clicks. This template is great for data exploration, model training, and more. Works on CPU and GPU hardware.",
-        author="nateraw",
+        authors=["camenduru", "nateraw"],
         url="https://huggingface.co/spaces/DockerTemplates/jupyterlab?duplicate=true",
         image_url="https://upload.wikimedia.org/wikipedia/commons/thumb/3/38/Jupyter_logo.svg/1767px-Jupyter_logo.svg.png",
         more_info="""
@@ -46,12 +57,12 @@ with gr.Blocks(css="style.css") as demo:
         - You can add dependencies to your JupyterLab instance by editing the `requirements.txt` file.
         - You can add linux packages to your JupyterLab instance by editing the `packages.txt` file.
         - You can add custom startup commands to your JupyterLab instance by editing the `on_startup.sh` file. These run with the root user.
-        """
+        """,
     )
     show_template(
         name="VSCode",
         description="Spin up a VSCode instance with just a couple clicks. This template is great for data exploration, model training, and more. Works on CPU and GPU hardware.",
-        author="nateraw",
+        authors=["camenduru", "nateraw"],
         url="https://huggingface.co/spaces/DockerTemplates/vscode?duplicate=true",
         image_url="https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/1200px-Visual_Studio_Code_1.35_icon.svg.png",
         more_info="""
