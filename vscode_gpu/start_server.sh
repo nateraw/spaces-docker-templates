2fd18c198138f56ee7493b08143bbd2666911c2d
Taylor Brown <taylor@youneedabudget.com>
2023-10-09 17:26:13 -0500

    Change VSCode install/run to run a tunnel instead

--- vscode_gpu/start_server.sh
+++ vscode_gpu/start_server.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
-echo "Starting VSCode Server..."
+# echo "Starting VSCode Server..."
+# exec /app/openvscode-server/bin/openvscode-server --host 0.0.0.0 --port 7860 --without-connection-token \"${@}\" --
 
-exec /app/openvscode-server/bin/openvscode-server --host 0.0.0.0 --port 7860 --without-connection-token \"${@}\" --
+echo "Downloading and starting the VSCode server"
+# TODO: Put this into the user directory instead of tmp
+# TODO: How to determine the name of the HF Space?
+mkdir -p ~/vscode
+trap '~/vscode/code tunnel unregister' EXIT
+cd ~/vscode && curl -Lk 'https://code.visualstudio.com/sha/download?build=stable&os=cli-alpine-x64' --output vscode_cli.tar.gz
+tar -xf vscode_cli.tar.gz
+~/vscode/code tunnel --accept-server-license-terms --no-sleep --name hf-space-dev-01
